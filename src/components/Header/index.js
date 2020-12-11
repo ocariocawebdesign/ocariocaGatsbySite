bbaafe483d0c4dd404a97254786c11705473604b
Carlos <ocariocawebdesign@gmail.com>
2020-12-11 14:38:38 -0300

    Cooreção de link no header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -83,7 +83,7 @@ const Header = () => (
               />
               <div className="p-sobre-2">
                 <a
-                  href="http://localhost:8000/post/estudo-de-objetos-em-javascript"
+                  href="https://ocariocawebdesign.com/estudo-de-objetos-em-javascript/"
                   target="_blank"
                   rel="noreferrer"
                 >
